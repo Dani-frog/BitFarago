62ee622cbb3c82aa4110896c9a2bce85eac92bef
Tóth Dániel János <toth.daniel@ckik.loc>
2023-09-21 10:40:51 +0200

    Elkezdtuk

---
+++ script.js
@@ -0,0 +1,22 @@
+
+var idozito;
+IdoKiiratas();
+idozito = setInterval(IdoKiiratas,1000);
+
+function IdoKiiratas(){
+    let Ma = new Date();
+    let Most=Ma.toLocaleString();
+    document.getElementById("Time").innerHTML = Most;
+}
+
+var i = 0;
+function Kepmutat()
+{
+    let t = ["Kep1","Kep2","Kep3"];
+    
+    document.getElementById(t[i++]).style.display="block";
+    if (i==t.length) {
+        
+    }
+   
+}
