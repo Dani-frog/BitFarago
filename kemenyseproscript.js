b3adf8e8b4f9ba8df2683dbdd6b715f9d585539a
Dani-frog <Danikataurusz@gmail.com>
2023-09-22 17:06:27 +0200

    Bemutatkozas majdnem kesz, kell visszavezető gomb meg bugfix

--- kemenyseproscript.js
+++ kemenyseproscript.js
@@ -14,7 +14,6 @@ function IdoKiiratas(){
 function Leiras(x)
 {
     const leirasok=["adass","adassanyad","adassapad","adasstesod","adassmamádasdddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd<br>dddddddddddddddddddddddddddddddddddddddddddadassmamádasdddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddadassmamádasdddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddadassmamádasddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd"]; 
-
     if (!megnyitva) {
         megnyitva = true;
         document.getElementById("Leiras").innerHTML=leirasok[x-1];
@@ -64,8 +63,10 @@ function GombokInaktiv(x){
 var i = 0;
 function KoviKep()
 {
-    let t = ["Kepek/Dani.png","Kepek/Boti.png","Kepek/Alex.png"];
-    document.getElementById("Slideshow").innerHTML = "<img src="+t[i++]+" class="+"SlideShow"+"></img>"
+    let t =["Kepek/Tajkepek/tajkep1.png","Kepek/Tajkepek/tajkep2.png","Kepek/Tajkepek/tajkep3.png",
+            "Kepek/Tajkepek/tajkep4.png","Kepek/Tajkepek/tajkep5.png","Kepek/Tajkepek/tajkep6.png"];
+
+    document.getElementById("Slideshow").innerHTML = "<img src="+t[i++]+" class="+"SlideShowalap"+"></img>"
     if (i==t.length)
         i = 0;
 }
