e62416f546233971d3dfb130df1adc20043a075c
Jakab Botond <jakab.botond@ckik.loc>
2023-10-09 11:13:40 +0200

    tejfol

--- Fordulo2/script.js
+++ Fordulo2/script.js
@@ -1,23 +1,41 @@
+var jatekTer = [["b","l","f","q","k","f","l","b"],
+                ["p","p","p","p","p","p","p","p"],
+                [" "," "," "," "," "," "," "," "],
+                [" "," "," "," "," "," "," "," "],
+                [" "," "," "," "," "," "," "," "],
+                [" "," "," "," "," "," "," "," "],
+                [" "," "," "," "," "," "," "," "],
+                [" "," "," "," "," "," "," "," "],
+                [" "," "," "," "," "," "," "," "],
+                [" "," "," "," "," "," "," "," "],
+                ["P","P","P","P","P","P","P","P"],
+                ["B","L","F","K","Q","F","L","B"]];
+
+function Main(){
+    TablaGen();
+}
+
+Main();
+
+
 
 function TablaGen() {
-    let feher = true;
-    for (let i = 0; i < 8; i++) {
+    let feher = false;
+    for (let i = 0; i < 12; i++) {
         feher = !feher;
-        for (let j = 0; j < 12; j++) {
+        for (let j = 0; j < 8; j++) {
             const cella = document.createElement("div");
             cella.classList.add("cella");
             cella.classList.add((feher) ? "feher" : "fekete");
             feher = !feher;
             cella.dataset.row = i;
             cella.dataset.col = j;
-            cella.setAttribute("onclick", "Kattint()");
+            cella.setAttribute("onclick", "Kattint(this)");
             document.getElementById("tabla").appendChild(cella);
         }
     }
 }
 
- function Main(){
-     TablaGen();
- }
+ function Kattint(){
 
- Main();
+ }
