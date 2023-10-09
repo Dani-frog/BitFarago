1c0f1ba99578ebbce6394d056aac1f467d03240f
Jakab Botond <jakab.botond@ckik.loc>
2023-10-09 12:15:14 +0200

    egészségünkre

--- Fordulo2/script.js
+++ Fordulo2/script.js
@@ -1,25 +1,54 @@
-var jatekTer = [["b","l","f","q","k","f","l","b"],
-                ["p","p","p","p","p","p","p","p"],
-                [" "," "," "," "," "," "," "," "],
-                [" "," "," "," "," "," "," "," "],
-                [" "," "," "," "," "," "," "," "],
-                [" "," "," "," "," "," "," "," "],
-                [" "," "," "," "," "," "," "," "],
-                [" "," "," "," "," "," "," "," "],
-                [" "," "," "," "," "," "," "," "],
-                [" "," "," "," "," "," "," "," "],
-                ["P","P","P","P","P","P","P","P"],
-                ["B","L","F","K","Q","F","L","B"]];
+const jatekTer = document.getElementById("jatekTer");
+var Mezok = [["b","l","f","q","k","f","l","b"],
+            ["p","p","p","p","p","p","p","p"],
+            [" "," "," "," "," "," "," "," "],
+            [" "," "," "," "," "," "," "," "],
+            [" "," "," "," "," "," "," "," "],
+            [" "," "," "," "," "," "," "," "],
+            [" "," "," "," "," "," "," "," "],
+            [" "," "," "," "," "," "," "," "],
+            [" "," "," "," "," "," "," "," "],
+            [" "," "," "," "," "," "," "," "],
+            ["P","P","P","P","P","P","P","P"],
+            ["B","L","F","K","Q","F","L","B"]];
+
 
 function Main(){
-    TablaGen();
+    if(document.getElementById("korbeInp").value != ''){
+        jatekTer.innerHTML = "";
+        TablaGen();
+    }else{
+        document.getElementById("korbeInp").placeholder = "Adj meg számot!";
+    }
+    
+}
+function KorValaszto(){
+    let korbeDiv = document.createElement("div");
+    korbeDiv.id = "korbeDiv"
+    let korbeKero = document.createElement("input");
+    korbeKero.id = "korbeInp";
+    korbeKero.type = "number";
+    korbeKero.min = 1;
+    let felirat = document.createElement("h2");
+    felirat.textContent = "Add meg a körszámot: ";
+    let gomb = document.createElement("button");
+    gomb.textContent = "Start";
+    gomb.setAttribute("onclick","Main()")
+    korbeDiv.appendChild(felirat);
+    korbeDiv.appendChild(korbeKero);
+    korbeDiv.innerHTML += "</br>";
+    korbeDiv.appendChild(gomb);
+    jatekTer.appendChild(korbeDiv);
+    
 }
 
-Main();
-
-
+KorValaszto();
 
 function TablaGen() {
+    let tablaDiv = document.createElement("div");
+    tablaDiv.id = "tabla";
+    tablaDiv.classList = "tabla";
+    jatekTer.appendChild(tablaDiv);
     let feher = false;
     for (let i = 0; i < 12; i++) {
         feher = !feher;
@@ -34,6 +63,7 @@ function TablaGen() {
             document.getElementById("tabla").appendChild(cella);
         }
     }
+    
 }
 
  function Kattint(){
